cfe1b008e494f494d59c9b5b38cc9fd0f151ee1c
Abd217391 <149207091+Abdullah0157@users.noreply.github.com>
2026-02-05 16:06:48 +0500

    test:updated bug type

--- types/bugs.ts
+++ types/bugs.ts
@@ -1,12 +1,19 @@
+
+
 export interface Bug {
   id: number;
   title: string;
   description?: string;
   status: string;
   type?: string;
-  created_by?: string | number;
-  created_at?: string;
   deadline?: string;
   screenshot_url?: string;
-  assignees: { name: string }[];
+  assignees?: { name: string }[];
+  // --- ADD THIS SECTION ---
+  sentiment?: {
+    label: string;
+    color: string;
+    bg: string;
+    icon: React.ReactNode;
+  };
 }
